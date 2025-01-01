90767d43f365d3b7f0d3ddeb7babb3dbd286af1f
Eghosa Ordia <eghordia130@gmail.com>
2025-01-01 23:05:34 +0100

    hello world render resolved

--- DayOne-SimpleReactApp/src/components/hello.tsx
+++ DayOne-SimpleReactApp/src/components/hello.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const HelloWorld = () => {
-  <>
-    <h1>Hello World</h1>
-  </>;
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
 };
 
-export default helloWorld;
+export default HelloWorld;
