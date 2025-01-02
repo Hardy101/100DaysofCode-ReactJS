f73623b67e6401f7aff8b3acb6e406a9284d5cda
Eghosa Ordia <eghordia130@gmail.com>
2025-01-02 12:23:40 +0100

    props using interface rerender

--- DayOne-SimpleReactApp/src/components/hello.tsx
+++ DayOne-SimpleReactApp/src/components/hello.tsx
@@ -1,18 +1,17 @@
 import React from "react";
+
 interface GreetingProps {
   name: String;
   age: number;
 }
 
-// const HelloWorld: React.FC<GreetingProps> = (props) => {
 const HelloWorld: React.FC<GreetingProps> = ({ name, age }) => {
   return (
     <>
-      <h1 className="text-3xl font-bold">
-        Hello {name}!, You have being alive for {age} years!
+      <h1 className="font-bold text-3xl">
+        Hello, {name}!, you have been alive for {age} years!
       </h1>
     </>
   );
 };
-
 export default HelloWorld;
