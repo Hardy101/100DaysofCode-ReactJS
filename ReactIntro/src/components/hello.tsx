83eafef8d2de862ae8f8621a1d2ca6cc9a97fa8c
Eghosa Ordia <eghordia130@gmail.com>
2025-01-03 14:39:14 +0100

    name change

--- ReactIntro/src/components/hello.tsx
+++ ReactIntro/src/components/hello.tsx
@@ -16,7 +16,7 @@ const HelloWorld: React.FC<GreetingProps> = ({ name, age }) => {
   const decrement = () => {
     setCount(count - 1);
   };
-  
+
   return (
     <>
       <main className="p-16">
@@ -24,10 +24,21 @@ const HelloWorld: React.FC<GreetingProps> = ({ name, age }) => {
           Hello, {name}!, you have been alive for {age} years!
         </h1>
         <p className="mt-4 text-2xl">
-          This number increases <span className="font-bold px-2 py-1 bg-[#f5f5f5]">{count}</span>
+          This number increases{" "}
+          <span className="font-bold px-2 py-1 bg-[#f5f5f5]">{count}</span>
         </p>
-        <button onClick={increment} className="text-sm px-3 py-2 bg-[#f5f5f5] mx-2 font-bold mt-5">Increase</button>
-        <button onClick={decrement} className="text-sm px-3 py-2 bg-[#f5f5f5] mx-2 font-bold">Decrease</button>
+        <button
+          onClick={increment}
+          className="text-sm px-3 py-2 bg-[#f5f5f5] mx-2 font-bold mt-5"
+        >
+          Increase
+        </button>
+        <button
+          onClick={decrement}
+          className="text-sm px-3 py-2 bg-[#f5f5f5] mx-2 font-bold"
+        >
+          Decrease
+        </button>
       </main>
     </>
   );
