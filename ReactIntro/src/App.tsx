5e914490329674f7d92eca72cd08fe88ca0e8f72
Eghosa Ordia <eghordia130@gmail.com>
2025-02-01 19:44:36 +0100

    modal redesign

--- ReactIntro/src/App.tsx
+++ ReactIntro/src/App.tsx
@@ -21,9 +21,9 @@ function App() {
         <BuggyComponent />
       </ErrorBoundary>
       ; */}
-      <div>
-        <h1>React Portals Example</h1>
-        <button onClick={() => setShowModal(true)}>Open Modal</button>
+      <div className="py-16 text-center">
+        <h1 className="font-bold mb-4 text-2xl">React Portals Example</h1>
+        <button className="bg-blue-100 text-blue-500 rounded-3xl font-bold py-2 px-4" onClick={() => setShowModal(true)}>Open Modal</button>
         {showModal && (
           <Modal onClose={() => setShowModal(false)}>
             <h2 className="mt-2">Modal Content</h2>
